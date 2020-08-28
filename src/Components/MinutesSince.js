448096aac82974456ee01ccb76fdfd55f80d7716
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-08-27 21:47:44 -0500

    DateFormat and imageIfNull

--- src/Components/MinutesSince.js
+++ src/Components/MinutesSince.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import '../CSS/MinutesSince.css';
-import {differenceInMinutes} from 'date-fns';
+import {formatDistance} from 'date-fns';
 
 
 function MinutesSince(props) {
 
     const getDifference = () =>{
 
-        const x = props.publishedAt;
-
+        const x = props.time;
         const dateTime = x.split("T");
-        const date = dateTime[0].split("-");
-
-        const time = dateTime[1].split(":");
-
+        const date = dateTime[0].split('-');
+        const time = dateTime[1].split(':');
+        const year = date[0];
+        const month = date[1];
+        const day = date[2];
+        const hour = time[0];
+        const min = time[1];
+        const sec = time[2].slice(0,-1);
         const rightNow = new Date();
 
-        const now = new Date(rightNow.getUTCFullYear(),rightNow.getUTCMonth(),rightNow.getUTCDay(),rightNow.getUTCHours(),rightNow.getUTCMinutes());
-        const published = new  Date(date[0],date[1],date[2],time[0],time[1]);
+        const myYear = rightNow.getUTCFullYear();
+        const myMonth = rightNow.getUTCMonth() + 1;
+        const myDay = rightNow.getUTCDate();
+        const myHour = rightNow.getUTCHours();
+        const myMin = rightNow.getUTCMinutes();
+        const mySec = rightNow.getUTCSeconds();
 
-        const y =  differenceInMinutes(now,published);
+        const y =  formatDistance(new Date(year,month,day,hour,min,sec),new Date(myYear,myMonth,myDay,myHour,myMin,mySec));
         return y;
-    }
+    }   
 
     
 
 
     return (
-        <div className="minutesSince">
-            <p>{getDifference()}</p>
-        </div>
+        
+        <>  {getDifference()} ago</>
+       
     )
 }
 
--- src/Components/Story.js
+++ src/Components/Story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../CSS/Story.css';
-
+import MinutesSince from './MinutesSince';
 
 class Story extends React.Component {
 
@@ -19,20 +19,22 @@ class Story extends React.Component {
     return (
         <div className="story"> 
             <div className="story__description">
-                <h4>{ (this.props.description === null) ? (this.props.title ): (this.props.description)}</h4>
+                <h4>{ (this.props.description === null || this.props.description === '') ? (this.props.title ): (this.props.description)}</h4>
                 
                 <ul>
-                    <li><p className="story__source">{this.props.source?.name}</p></li>
+                    
                     <li>
                         <p>
                         {this.props.title}
                         </p>
                     </li>
+                    <li><p className="story__source">{this.props.source?.name}<MinutesSince time={this.props.publishedAt} ></MinutesSince></p></li>
                 </ul>
                 
             </div>
             <div className="story__image__div">
-            <img src={this.props.urlToImage} alt="storyMeme" className="test_image" />          
+                {(this.props.urlToImage === null || this.props.urlToImage === '') ? (<img src={this.props.urlToImage} alt="storyMeme" className="test_image" style={{display: 'none'}} />   ): (<img src={this.props.urlToImage} alt="storyMeme" className="test_image" />   )}
+                   
             </div> 
         </div>
         
